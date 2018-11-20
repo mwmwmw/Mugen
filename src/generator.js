e6624a0620d5b63265b945f96755836d966f9484
Matthew Willox <matthew@gskinner.com>
2018-11-19 17:41:19 -0700

    Build

--- src/generator.js
+++ src/generator.js
@@ -6,55 +6,7 @@ import { generateScale } from "./scale";
 
 import { generateRhythm } from "./rhythm";
 
-export const pickMelodyNotes = scale => {
-  let notes = scale.slice();
-  return notes.filter(n => {
-    if (check(0.5)) return false;
-    return true;
-  });
-};
 
-export const calculateSequence = (
-  notes,
-  length = 16,
-  structure = generateMelodyStructure(),
-  offset = 0
-) => {
-  let sequence = new Array(length).fill(-1);
-  let index = -1;
-  let prog = length / 4;
-  sequence.map((n, i) => {
-    let beat = i / length;
-    let gen = Math.random();
-    if (gen > beat) {
-      index++;
-    } else if (gen < 0.33333) {
-      index--;
-    } else {
-      index = Math.floor(Math.random() * notes.length);
-    }
-    if (index < 0) {
-      index += notes.length;
-    }
-
-    sequence[i] = structure[i % structure.length]
-      ? notes[index % notes.length] + offset
-      : -1;
-  });
-  return sequence;
-};
-
-export const mutatePattern = (pattern, notes) => {
-  let newPattern = pattern.slice();
-
-  newPattern.map((n, i) => {
-    newPattern[i] = check(i / newPattern.length)
-      ? newPattern[i]
-      : notes[Math.floor(Math.random() * notes.length)];
-  });
-
-  return newPattern;
-};
 
 const SONG_PART = {
   A: "A",
