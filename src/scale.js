ada1464eef831035d1906448b366257e690094b5
Matthew Willox <skintilla@gmail.com>
2018-11-20 08:37:33 -0700

    Cleaned up scale generation code to be more intuitive

--- src/scale.js
+++ src/scale.js
@@ -1,10 +1,16 @@
 
 export const generateScale = (semitones, rootNote = 60, range = 24) => {
-    let notes = [rootNote];
+    let notes = [];
     for (let i = 0; i < range; i++) {
+      notes.push(rootNote);
       const inc = semitones[i % semitones.length];
       rootNote += inc;
-      notes.push(rootNote);
     }
     return notes;
   }; 
+
+
+  export const transpose = (notes, amountInSemitones) => {
+      let newNotes = notes.slice();
+      return newNotes.map((n)=>n+amountInSemitones); 
+  }
