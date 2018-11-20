7c550fba87d430d9c3a018954b2993620085af17
Matthew Willox <matthew@gskinner.com>
2018-11-19 17:14:18 -0700

    Rhythm updates

--- src/rhythm.js
+++ src/rhythm.js
@@ -1,18 +1,20 @@
-import {check} from "./constants";
- 
-export const generateRhythm = (beats = 4, measure = 4) => {
-    let sequence = new Array(beats * measure).fill(0);
-    sequence.map((n, i) => {
-      let pulse = 1 / ((i % beats) + 1);
-      sequence[i] = check(pulse);
-    });
-    return sequence;
-  };
-  
-  export const syncopate = (rhythm) => {
-    let newRythm = rhythm.slice();
-    newRythm.map((v)=>{
-        return v ? !v : check(Math.random())
-    })
+import {
+  check
+} from "./constants";
 
-  }
+export const generateRhythm = (beats = 4, measure = 4) => {
+  let sequence = new Array(beats * measure).fill(0);
+  sequence.map((n, i) => {
+    let pulse = 1 / ((i % beats) + 1);
+    sequence[i] = check(pulse);
+  });
+  return sequence;
+};
+
+export const syncopate = (rhythm) => {
+  let newRythm = rhythm.slice();
+  newRythm.map((v) => {
+    return v ? !v : check(Math.random())
+  })
+
+}
