6b9d1f47de6a1ea2c98428b9bcaf3a6fd661d9e4
Matthew Willox <matthew@gskinner.com>
2018-11-20 17:23:32 -0700

    proper exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import { check } from "./constants";
 
-export { computeChords, identifyChordType } from "./chords";
-export { generateScale } from "./scale"; 
-export { generateSong } from "./generator";  
+import * as chords from "./chords";
+import * as scale from "./scale"; 
+import * as melody from "./melody"; 
+//export { generateSong } from "./generator";  
+
+import  * as utils from "./constants";
+
+export { chords, scale, utils, melody };
