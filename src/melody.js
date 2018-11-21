9bf3383dee9c9909b491291e4db9c831c1f976fc
Matthew Willox <matthew@gskinner.com>
2018-11-20 17:23:22 -0700

    Melody updates

--- src/melody.js
+++ src/melody.js
@@ -1,61 +1,53 @@
-import { check } from "./constants";
+import {
+    check,
+    shuffle
+} from "./constants";
 
 
-export const pickRandomNotes = (scale, probability) => {
-  let notes = scale.slice();
-  return notes.filter(n => {
-    if (check(probability)) return false;
-    return true;
-  });
+export const filterRandomNotes = (notes, probability = 0.5) => {
+    return notes.filter(n => check(probability));
+}
+
+export const randomNote = (notes) => {
+    return shuffle(notes)[0];
 }
 
 export const mutatePattern = (pattern, notes) => {
-  let newPattern = pattern.slice();
-
-  newPattern.map((n, i) => {
-    newPattern[i] = check(i / newPattern.length)
-      ? newPattern[i]
-      : notes[Math.floor(Math.random() * notes.length)];
-  });
-
-  return newPattern;
+    let newPattern = pattern.slice();
+        newPattern.map((n, i) => {
+            return check(i / newPattern.length) ?
+                newPattern[i] :
+                notes[Math.floor(Math.random() * notes.length)];
+        });
+ return newPattern;
 };
 
-export const pickMelodyNotes = scale => {
-    let notes = scale.slice();
-    return notes.filter(n => {
-      if (check(0.5)) return false;
-      return true;
-    });
-  };
-  
-  export const calculateSequence = (
+export const calculateSequence = (
     notes,
     length = 16,
     structure = generateMelodyStructure(),
     offset = 0
-  ) => {
+) => {
     let sequence = new Array(length).fill(-1);
     let index = -1;
     let prog = length / 4;
     sequence.map((n, i) => {
-      let beat = i / length;
-      let gen = Math.random();
-      if (gen > beat) {
-        index++;
-      } else if (gen < 0.33333) {
-        index--;
-      } else {
-        index = Math.floor(Math.random() * notes.length);
-      }
-      if (index < 0) {
-        index += notes.length;
-      }
-  
-      sequence[i] = structure[i % structure.length]
-        ? notes[index % notes.length] + offset
-        : -1;
+        let beat = i / length;
+        let gen = Math.random();
+        if (gen > beat) {
+            index++;
+        } else if (gen < 0.33333) {
+            index--;
+        } else {
+            index = Math.floor(Math.random() * notes.length);
+        }
+        if (index < 0) {
+            index += notes.length;
+        }
+
+        sequence[i] = structure[i % structure.length] ?
+            notes[index % notes.length] + offset :
+            -1;
     });
     return sequence;
-  };
-  
+};
